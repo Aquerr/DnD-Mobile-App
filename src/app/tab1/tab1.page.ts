cef1f14a227616ee615c95f905c8a0b891b265da
Aquerr <nerdianin@gmail.com>
2022-09-21 22:44:48 +0200

    Experimenting with tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  abilityScores: AbilityScore[] = [
+    new AbilityScore("Strength", 8, 2),
+    new AbilityScore("Intelligence", 13, 1),
+    new AbilityScore("Dexterity", 20, 8),
+    new AbilityScore("Wisdom", 14, 2),
+    new AbilityScore("Constitution", 14, 2),
+    new AbilityScore("Charisma", 11, 0)
+  ];
 
+  constructor() {}
+}
+
+export class AbilityScore {
+  name: string;
+  score: number;
+  save: number;
+
+  constructor(name: string, score: number, save: number) {
+    this.name = name;
+    this.score = score;
+    this.save = save;
+  }
+
+  get modifier() {
+    return this.score / 2 - 5;
+  }
+
+  // modifier(): number {
 }
